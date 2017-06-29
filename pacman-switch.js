614e5ecf4d981f8d383bdd15008a0c5f599713cc
Lia Forgione <liaforg@gmail.com>
2017-06-28 20:59:28 -0400

    pacman switch

---
+++ pacman-switch.js
@@ -0,0 +1,16 @@
+var ghost = 'Clyde';
+var colour;
+
+switch (ghost) {
+  case 'Inky':
+    colour = 'Cyan';
+    break;
+  case 'Blinky':
+    colour = 'Red';
+    break;
+  case 'Clyde':
+    colour = 'Orange';
+    break;
+}
+
+console.log(ghost + ' is the colour ' + colour);
