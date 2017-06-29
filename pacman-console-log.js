4d1ba15ebdcdfdd04818d43bfba3ea27ae0e336e
Lia Forgione <liaforg@gmail.com>
2017-06-28 20:46:18 -0400

    pacman arrays, console-log, loops, and objects 1 and 2

--- pacman-console-log.js
+++ pacman-console-log.js
@@ -1,1 +1,9 @@
 console.log('Pac-Man eats ghosts for lunch.');
+
+//I am a comment!
+var ghost = "Blinky!"; //comments
+/*
+comment
+comment
+comment
+*/
---
+++ pacman-objects-2.js
@@ -0,0 +1,5 @@
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
+console.log(ghosts);
+
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
